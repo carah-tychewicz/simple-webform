5117731942a1a388aa43ef734df5cd277be181b7
carah-tychewicz <10439123+carah-tychewicz@users.noreply.github.com>
2022-08-03 21:27:29 -0500

    set up page routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Form from './components/Form';
+import ThankYou from './components/ThankYou';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Form />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Form />} />
+        <Route path="thank-you" element={<ThankYou />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
