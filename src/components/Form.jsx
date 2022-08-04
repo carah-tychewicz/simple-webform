29f1bc0d0a1844f83bac3c037f1114dbd3ce2642
carah-tychewicz <10439123+carah-tychewicz@users.noreply.github.com>
2022-08-03 19:57:39 -0500

    basic form built. going to make it functional then stylize towards last stage of development.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,37 @@
 import './Form.css';
 
+// Fields: Name, Email, Address, City, State, Zip
+
 const Form = () => {
   return (
     <div className='container'>
       <div className='form-card'>
-        <form>
-          <div>
-            <label>test</label>
-            <input type='text' name='test' />
+        <form className='form-col'>
+          <div className='form-item'>
+            <label>Full Name</label>
+            <input type='text' name='fullName' />
           </div>
-          <button type='submit'>Submit</button>
+          <div className='form-item'>
+            <label>Email</label>
+            <input type='text' name='email' />
+          </div>
+          <div className='form-item'>
+            <label>Address</label>
+            <input type='text' name='address' />
+          </div>
+          <div className='form-item'>
+            <label>City</label>
+            <input type='text' name='city' />
+          </div>
+          <div className='form-item'>
+            <label>State</label>
+            <input type='text' name='state' />
+          </div>
+          <div className='form-item'>
+            <label>Zip Code</label>
+            <input type='text' name='zip' />
+          </div>
+          <button className='form-submit-btn' type='submit'>Submit</button>
         </form>
       </div>
     </div>
