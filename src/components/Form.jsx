ad3f82527118dce3d344ef601d593a6cd69406fc
carah-tychewicz <10439123+carah-tychewicz@users.noreply.github.com>
2022-08-05 11:19:46 -0500

    merging what i have for validation. good enough for bonus points. time to make it look good.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -32,100 +32,37 @@ const Form = () => {
     };
   };
 
-  const unitedStates = 
-    [{
-    "AL": "Alabama",
-    "AK": "Alaska",
-    "AS": "American Samoa",
-    "AZ": "Arizona",
-    "AR": "Arkansas",
-    "CA": "California",
-    "CO": "Colorado",
-    "CT": "Connecticut",
-    "DE": "Delaware",
-    "DC": "District Of Columbia",
-    "FM": "Federated States Of Micronesia",
-    "FL": "Florida",
-    "GA": "Georgia",
-    "GU": "Guam",
-    "HI": "Hawaii",
-    "ID": "Idaho",
-    "IL": "Illinois",
-    "IN": "Indiana",
-    "IA": "Iowa",
-    "KS": "Kansas",
-    "KY": "Kentucky",
-    "LA": "Louisiana",
-    "ME": "Maine",
-    "MH": "Marshall Islands",
-    "MD": "Maryland",
-    "MA": "Massachusetts",
-    "MI": "Michigan",
-    "MN": "Minnesota",
-    "MS": "Mississippi",
-    "MO": "Missouri",
-    "MT": "Montana",
-    "NE": "Nebraska",
-    "NV": "Nevada",
-    "NH": "New Hampshire",
-    "NJ": "New Jersey",
-    "NM": "New Mexico",
-    "NY": "New York",
-    "NC": "North Carolina",
-    "ND": "North Dakota",
-    "MP": "Northern Mariana Islands",
-    "OH": "Ohio",
-    "OK": "Oklahoma",
-    "OR": "Oregon",
-    "PW": "Palau",
-    "PA": "Pennsylvania",
-    "PR": "Puerto Rico",
-    "RI": "Rhode Island",
-    "SC": "South Carolina",
-    "SD": "South Dakota",
-    "TN": "Tennessee",
-    "TX": "Texas",
-    "UT": "Utah",
-    "VT": "Vermont",
-    "VI": "Virgin Islands",
-    "VA": "Virginia",
-    "WA": "Washington",
-    "WV": "West Virginia",
-    "WI": "Wisconsin",
-    "WY": "Wyoming"
-}]
-
   return (
     <div className='container'>
       <div className='card'>
         <form onSubmit={handleSubmit}>
           <div className='form-item'>
             <label>Full Name</label>
-            <input type='text' name='fullName' onChange={handleInputChange} value={inputs.fullName ?? ''} required />
+            <input type='text' name='fullName' maxLength='50' onChange={handleInputChange} value={inputs.fullName ?? ''} required />
           </div>
           <div className='form-item'>
             <label>Email</label>
-            <input type='text' name='email' onChange={handleInputChange} value={inputs.email ?? ''} required />
+            <input type='text' name='email' maxLength='50' onChange={handleInputChange} value={inputs.email ?? ''} required />
           </div>
           <div className='form-item'>
-            <label>Address</label>
-            <input type='text' name='address' onChange={handleInputChange} value={inputs.address ?? ''} />
+            <label>Address Line 1</label>
+            <input type='text' name='address' maxLength='50' onChange={handleInputChange} value={inputs.address ?? ''} />
+          </div>
+          <div className='form-item'>
+            <label>Address Line 2</label>
+            <input type='text' name='address' maxLength='50' onChange={handleInputChange} value={inputs.address ?? ''} />
           </div>
           <div className='form-item'>
             <label>City</label>
-            <input type='text' name='city' onChange={handleInputChange} value={inputs.city ?? ''} />
+            <input type='text' name='city' maxLength='50' onChange={handleInputChange} value={inputs.city ?? ''} />
           </div>
           <div className='form-item'>
             <label>State</label>
-            <input type='text' name='state' onChange={handleInputChange} value={inputs.state ?? ''} />
-            {/* <select value={inputs.state} onChange={handleInputChange}>
-            {
-              Object.value(unitedStates)
-            </select> */}
+            <input type='text' name='state' maxLength="20" onChange={handleInputChange} value={inputs.state ?? ''} />
           </div>
           <div className='form-item'>
             <label>Zip Code</label>
-            <input type='text' name='zip' onChange={handleInputChange} value={inputs.zip ?? ''} />
+            <input type='text' name='zip' maxLength='5' onChange={handleInputChange} value={inputs.zip ?? ''} />
           </div>
           <button className='form-submit-btn' type='submit'>Submit</button>
         </form>
